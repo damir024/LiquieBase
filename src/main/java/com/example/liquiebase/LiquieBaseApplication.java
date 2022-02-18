6349532f822d8d1121cf78c932c5194bd38dee28
damirumanets <damirumanets044@gmail.com>
2022-02-18 14:18:35 +0200

    Initial commit

---
+++ src/main/java/com/example/liquiebase/LiquieBaseApplication.java
@@ -0,0 +1,13 @@
+package com.example.liquiebase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiquieBaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LiquieBaseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/liquiebase/LiquieBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.liquiebase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiquieBaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
